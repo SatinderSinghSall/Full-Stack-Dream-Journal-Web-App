a539c3fc8a7a556a1ead0a26ac61bad0b5857725
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-10-21 17:02:28 +0530

    Bugs fixed.

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -2,11 +2,14 @@ const nodemailer = require("nodemailer");
 
 exports.sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 10000,
   });
 
   const mailOptions = {
